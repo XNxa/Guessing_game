4f0f0d3679cbdc2904f44304bc8f7b5b029f59a5
Xavier Naxara <xaviernaxara@yahoo.fr>
2023-08-12 16:01:22 +0200

    Added min/max choice + Factorized code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
 use rand::{thread_rng, Rng};
-use std::io;
+use std::{io, str::FromStr, fmt::Display};
 
-const MIN: i32 = 1;
-const MAX: i32 = 1000;
+fn ask_a_number<T : FromStr>(msg: &str) -> T 
+    where <T as FromStr>::Err: Display 
+{
+    loop {
+        println!("{}", msg);
+        
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("A message should be written");
+
+        match input.trim().parse::<T>() {
+            Ok(ans) => {return ans;},
+            Err(err) => {
+                eprintln!("Error : {}", err);
+                continue;
+            },
+        }
+    }
+}
 
 fn main() {
 
     // Number generator
     let mut rng = thread_rng();
     
-    // Choose a number randomly
-    let n = rng.gen_range(MIN..=MAX);
     
-    // do while not the right number
+    // Asking for min and max
+    let mut min:i32;
+    let mut max:i32;
     loop {
-        // Ask user a number
-        println!("Choose a number between {MIN} and {MAX} : ");
+        min = ask_a_number("Please enter the min value : ");
+        max = ask_a_number("Please enter the max value : ");
         
-        // IO
-        let mut input = String::new();
-        let stdin = io::stdin();
-        stdin.read_line(&mut input).expect("A message should be writen");
+        if min < max {break;} else {
+            println!("Max value must be greater than min value. Please try again.")
+        }
+    }
 
-        let choosen = match input.trim().parse::<i32>() {
-            Ok(number) => number,
-            Err(err) => {
-                eprintln!("You should write a number : {err}");
-                continue;
-            },
-        };
+    // Choose a number randomly
+    let n = rng.gen_range(min..=max);
+    
+    // do while (not the right number)
+    loop {
+        let choosen:i32 = ask_a_number(&format!("Choose a number between {min} and {max} : "));
 
         // Compare and tell the user
         if choosen < n {
